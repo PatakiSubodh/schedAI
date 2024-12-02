04e7176f9d3caf43c06a311da07b61e33dff30d9
tanmay <tanmayrnetrakar@gmail.com>
2024-12-02 15:54:54 +0530

    third commit

--- projttgs/account/urls.py
+++ projttgs/account/urls.py
@@ -13,7 +13,8 @@ urlpatterns = [
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
     path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
-    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done')
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
 ]
 
 
